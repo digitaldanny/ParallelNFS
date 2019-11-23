bf7ab3b6abe50779ccd92e533f71760805bf1666
Daniel Hamilton <d.hamilton@ufl.edu>
2019-11-23 17:31:22 -0500

    shut down all servers from command line

--- project/backChannel.py
+++ project/backChannel.py
@@ -11,6 +11,7 @@ import xmlrpclib, config, pickle, os, sys, subprocess, time
 proxy = []
 num_servers   	= sys.argv[1]
 num_servers   	= int(num_servers)
+p               = [None for i in range(num_servers)] # subprocess handles for closing terminal windows
 print("Number of servers: " + str(num_servers))
 portNum 		= 8000
 
@@ -20,16 +21,24 @@ operating on portNum + i. This is how the file system will be able to specify
 which server it will communicate with.
 '''
 for i in range(num_servers) :
-	print('running server #' + str(portNum+i))
-	proxy.append(xmlrpclib.ServerProxy("http://localhost:" + str(portNum + i) + "/"))
-	os.system('gnome-terminal -e \"python server.py ' + str(portNum + i) + '\"')
-	time.sleep(1)
+    print('running server #' + str(portNum+i))
+    proxy.append(xmlrpclib.ServerProxy("http://localhost:" + str(portNum + i) + "/"))
+    os.system('gnome-terminal -e \"python server.py ' + str(portNum + i) + '\"')
+    #time.sleep(1)
 
-while False:
-	serverNum = int(raw_input("Select Server to Corrupt..."))
-	try :
-		retVal =  proxy[serverNum].corruptData()
-		print(serverNum)
-		print(pickle.loads(retVal))
-	except Exception as err :
-		print('connection error')
+while True:
+    serverNum = int(raw_input("Select Server to Corrupt..."))
+    try :
+        retVal =  proxy[serverNum].corruptData()
+        print(serverNum)
+        print(pickle.loads(retVal))
+        
+    except Exception as err :
+        print('Connection error.. closing all servers.')
+        for i in range(num_servers):
+            print("Killing server on port " + str(portNum + i))
+            try:
+                proxy[i].kill()
+            except:
+                pass
+        break
